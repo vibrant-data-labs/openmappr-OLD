6912d66160c584c6e8047748ce00917276583104
mandeep <mandeep.s.gulati@gmail.com>
2018-07-29 16:02:56 +0530

    changes to rebuild script

--- rebuild-web.sh
+++ rebuild-web.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-docker-compose pause
 docker-compose stop web
 docker-compose rm -f web
 docker-compose build --no-cache web
-docker-compose up -d web
-docker-compose unpause
+docker-compose up -d
