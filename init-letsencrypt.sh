cbe11df2ff0e492b873b1c0170d2e0e3801952b4
Abhi <abhimediratta@gmail.com>
2019-11-26 23:26:00 +0530

    use prod environment for cerbot script - change nginx conf

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -4,7 +4,7 @@ set -e
 domains=(app.openmappr.org) # Specify domains here or use the -d argument
 data_path="./data/certbot" # Specify data path here or use the --data-path argument
 email="abhimediratta@gmail.com" # Specify email here or use the --email argument
-staging=1 # Set to 1 here or use the --staging argument
+staging=0 # Set to 1 here or use the --staging argument
 rsa_key_size=4096
 
 print_help() {
