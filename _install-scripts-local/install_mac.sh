27ac6dee620777825e5ca9eebc02b12a85ef2619
Yale Fox <yalefox@gmail.com>
2020-04-11 02:07:31 -0400

    Update install_mac.sh

--- _install-scripts-local/install_mac.sh
+++ _install-scripts-local/install_mac.sh
@@ -1,45 +1,63 @@
 #!/bin/bash
+# 
+# This script assumes you have nothing installed. It will skip over anything you don't need.
+# 
+#
+#
+# Install Xcode as unattended first
 
-#This Script will assume you have Docker & Docker Compose installed
-#Please follow the latest instructions on intalling this libraries:
-# https://docs.docker.com/install/
-# https://docs.docker.com/compose/install/
 
-# Node version 8.12 is also required
-# nvm is recommended but n is another option
-# Visit NVM for instructions on install
-# https://github.com/nvm-sh/nvm#installing-and-updating
+####################
 
-echo Running docker Compose scripts
+echo "Installing Homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-docker-compose -f docker-compose-local.yml up -d
-docker-compose -f docker-compose-local.yml ps
+echo "Installing Node Version Manager (NVM)"
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
 
-echo installling Node.js version 8.12.0
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+# reload the system environment
+echo "Reloading system environment"
+source ~/.zshrc
+
+# Install NodeJS 8.12 and switch to it
+echo "Installing NodeJS v8.12.0 (openmappr dependency)"
 nvm install 8.12.0
-node --version
 
-clear
+echo "Switching to NodeJS v8.12.0"
+nvm use 8.12.0
 
-echo installing gems
-echo compass:
+
+# Install dependencies 
+echo "Installing docker"
+brew install docker 
+
+echo "Installing dependencies"
+brew install git curl
+
+echo "Cloning openmappr repo"
+git clone https://github.com/selfhostedofficial/openmappr
+cd openmappr
+
+# Install the compass Gem
+echo "installing compass gem"
 gem install compass
 
-clear
-
-echo installing dependencies
-
+echo "Installing bower and grunt"
 sudo npm install -g yo bower grunt-cli
 npm install
 bower install
 
-clear
-
-echo building app
-
+# Build the application
+echo "Building application"
 grunt
 
-echo running server at PORT: http://localhost:8080
+echo "running server at PORT: http://localhost:8080"
 ./run_local_mode.sh
+
+# Open a browser window
+# echo "opening https://localhost:8080"
+# open https://localhost:8080
+
+# echo running server at PORT: http://localhost:8080
+# ./run_local_mode.sh
 
