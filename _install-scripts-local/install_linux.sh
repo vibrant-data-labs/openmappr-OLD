dab86f14df65a934c8f0b79ea6783687d5173b5c
Yale Fox <yalefox@gmail.com>
2020-04-11 03:58:16 -0400

    yale's scripts (incomplete)

--- _install-scripts-local/install_linux.sh
+++ _install-scripts-local/install_linux.sh
@@ -1,33 +1,52 @@
 #!/bin/bash
+#
+#        The only thing you need to install here is cURL.
+#             `sudo apt install curl -y` 
+#             To do so
+# Get updates
+sudo apt update -y
+sudo apt dist-upgrade -y
 
-#This Script will assume you have Docker & Docker Compose installed
-#Please follow the latest instructions on intalling this libraries:
-# https://docs.docker.com/install/
-# https://docs.docker.com/compose/install/
+# remove old versions of docker dependencies
+sudo apt remove docker docker-engine docker.io containerd runc -y 
 
-# Node version 8.12 is also required
-# nvm is recommended but n is another option
-# Visit NVM for instructions on install
-# https://github.com/nvm-sh/nvm#installing-and-updating
+# Install dependencies
+sudo apt update -y 
 
+# Install docker
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
 
-# bring up all the docker services
-echo
-echo "Bring up all the docker services."
-echo
-sleep 2
-docker-compose -f docker-compose-local.yml up -d
+# Install docker-compose
+sudo apt install docker-compose -y
 
-# install yo bower grunt-cli
-echo
-echo "Install nvm and node 8.12."
-echo
-sleep 2
+# Download Node Version Manager
+echo "Downloading Node Version Manager"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-nvm install 8.12
 
-gem install sass
-gem install compass
+# Reload the system environment
+echo "Reloading system environment"
+source ~/.profile
+
+# Install Node Version 8.12
+echo "Installing Node version 8.12"
+nvm install 8.12.0
+
+# Switch to Node Node Version 8.12
+echo "Switching to Node v 8.12.0"
+nvm use 8.12.0
+
+# Clone openMappr repo
+echo "Cloning openmappr repo"
+git clone https://github.com/selfhostedofficial/openmappr
+cd openmappr
+
+
+
+
+
+# gem install sass
+# gem install compass
 
 # install yo bower grunt-cli
 echo
@@ -46,5 +65,13 @@ bower install
 
 grunt
 
-./run_local_mode.sh
+sudo sh run_local_mode.sh
+
+
+# bring up all the docker services
+echo
+echo "Bring up all the docker services."
+echo
+sleep 2
+docker-compose -f docker-compose-local.yml up -d
 
--- _install-scripts-local/install_mac.sh
+++ _install-scripts-local/install_mac.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 # 
 # This script assumes you have nothing installed. It will skip over anything you don't need.
-# 
-#
-#
-# Install Xcode as unattended first
-
 
 ####################
 
@@ -13,7 +8,7 @@ echo "Installing Homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 echo "Installing Node Version Manager (NVM)"
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
 # reload the system environment
 echo "Reloading system environment"
@@ -26,21 +21,28 @@ nvm install 8.12.0
 echo "Switching to NodeJS v8.12.0"
 nvm use 8.12.0
 
-
 # Install dependencies 
 echo "Installing docker"
-brew install docker 
+brew cask install docker
+open /Applications/Docker.app
 
 echo "Installing dependencies"
 brew install git curl
 
+# Clone openMappr repo
 echo "Cloning openmappr repo"
 git clone https://github.com/selfhostedofficial/openmappr
 cd openmappr
 
+# Install ruby
+brew install ruby
+
 # Install the compass Gem
 echo "installing compass gem"
 gem install compass
+gem install sass
+
+# npm install -g sass
 
 echo "Installing bower and grunt"
 sudo npm install -g yo bower grunt-cli
@@ -54,10 +56,4 @@ grunt
 echo "running server at PORT: http://localhost:8080"
 ./run_local_mode.sh
 
-# Open a browser window
-# echo "opening https://localhost:8080"
-# open https://localhost:8080
-
-# echo running server at PORT: http://localhost:8080
-# ./run_local_mode.sh
-
+# docker-compose -f docker-compose-local.yml up -d
