386419372fe40fd66e8a721ea80971bde41c0fd4
Abhi Mediratta <abhimediratta@gmail.com>
2020-04-18 22:31:00 +0530

    Revert "hover-select-subset (#46)" (#47)
    This reverts commit bfd7ab6a87a9ab76d609477d0e26fd57131ac2b4.

--- client/src/components/project/panels/right_panel/info_panel/dirNodesList.js
+++ client/src/components/project/panels/right_panel/info_panel/dirNodesList.js
@@ -37,134 +37,91 @@ function(BROADCAST_MESSAGES, graphHoverService, graphSelectionService, FilterPan
     /*************************************
     ******** Post Link Function *********
     **************************************/
-            function postLinkFn(scope, elem, attrs, parCtrl) {
+    function postLinkFn(scope, elem, attrs, parCtrl) {
 
-                var memoizedGetFunctionColor = _.memoize(getFunctionColor);
+        var memoizedGetFunctionColor = _.memoize(getFunctionColor);
 
-                scope.nodeSearchQuery = '';
-                scope.numShowGroups = 0;
-                scope.viewLimit = Math.min(ITEMS_TO_SHOW_INITIALLY, scope.nodes.length);
+        scope.nodeSearchQuery = '';
+        scope.numShowGroups = 0;
+        scope.viewLimit = Math.min(ITEMS_TO_SHOW_INITIALLY, scope.nodes.length);
 
-                layoutService.getCurrent().then(function (layout) {
-                    scope.layout = layout;
-                });
+        layoutService.getCurrent().then(function (layout) {
+            scope.layout = layout;
+        });
 
-                scope.$watch('nodes', function() {
-                    scope.viewLimit = Math.min(ITEMS_TO_SHOW_INITIALLY, scope.nodes.length);
-                });
+        scope.$watch('nodes', function() {
+            scope.viewLimit = Math.min(ITEMS_TO_SHOW_INITIALLY, scope.nodes.length);
+        });
 
-                scope.viewMore = function() {
-                    if(scope.viewLimit < scope.nodes.length) {
-                        // scope.viewLimit += Math.min(minViewCount, scope.nodes.length - scope.viewLimit);
-                        scope.numShowGroups++;
-                        scope.viewLimit = Math.min(scope.nodes.length, scope.numShowGroups * ITEMS_TO_SHOW + ITEMS_TO_SHOW_INITIALLY);
-                    }
-                };
+        scope.viewMore = function() {
+            if(scope.viewLimit < scope.nodes.length) {
+                // scope.viewLimit += Math.min(minViewCount, scope.nodes.length - scope.viewLimit);
+                scope.numShowGroups++;
+                scope.viewLimit = Math.min(scope.nodes.length, scope.numShowGroups * ITEMS_TO_SHOW + ITEMS_TO_SHOW_INITIALLY);
+            }
+        };
 
-                scope.viewLess = function() {
-                    scope.numShowGroups = 0;
-                    scope.viewLimit = Math.min(ITEMS_TO_SHOW_INITIALLY, scope.nodes.length);
-                };
+        scope.viewLess = function() {
+            scope.numShowGroups = 0;
+            scope.viewLimit = Math.min(ITEMS_TO_SHOW_INITIALLY, scope.nodes.length);
+        };
 
-                scope.selectNode = function(node, $event) {
-                    // if(scope.panelMode == 'selection') {
-                    //     parCtrl.openNodeBrowserInSelMode();
-                    // }
+        scope.selectNode = function(nodeId, $event) {
+            if(scope.panelMode == 'selection') {
+                parCtrl.openNodeBrowserInSelMode();
+            }
+            selectNodes([nodeId], $event);
+        };
 
-                    highlightNodes([node]);
-                };
-                //
+        scope.hoverNode = function(nodeId) {
+            hoverNodes([nodeId]);
+        };
 
-                scope.hoverNode = function(node) {
-                    hoverNodes([node]);
-                };
+        scope.unHoverNode = function(nodeId) {
+            unHoverNodes([nodeId]);
+        };
 
-                scope.unHoverNode = function(node) {
-                    unHoverNodes([node]);
-                };
+        scope.selectGroup = function(group) {
+            selectNodes(_.map(group.nodes, 'id'));
+        };
 
-                scope.selectGroup = function(group) {
-                    // selectNodes(_filterNodes(group.name));
-                    highlightNodes(_filterNodes(group.name));
-                };
+        scope.hoverGroup = function(group) {
+            hoverNodes(_.map(group.nodes, 'id'));
+        };
 
-                scope.hoverGroup = function(group) {
-                    // hoverNodes(_.map(group.nodes, 'id'));
-                    hoverNodes(_filterNodes(group.name));
-                };
+        scope.unHoverGroup = function(group) {
+            unHoverNodes(_.map(group.nodes, 'id'));
+        };
 
-                scope.unHoverGroup = function(group) {
-                    // unHoverNodes(_.map(group.nodes, 'id'));
-                    unHoverNodes(_filterNodes(group.name));
-                };
+        scope.filterNode = function(node) {
+            if (!scope.nodeSearchQuery) { return true; }
+            var regex = new RegExp(scope.nodeSearchQuery, 'gi');
+            return node.attr[scope.labelAttr].match(regex);
+        };
 
-                scope.filterNode = function(node) {
-                    if (!scope.nodeSearchQuery) { return true; }
-                    var regex = new RegExp(scope.nodeSearchQuery, 'gi');
-                    return node.attr[scope.labelAttr].match(regex);
-                };
+        scope.getNodeTooltipHtml = function(node) {
+            var html = '<ul class="list-unstyled">';
+            var attrTitle = _.get(_.find(scope.sortTypes, 'id', scope.sortInfo.sortType), 'title', '');
+            var attrVal = node.attr[scope.sortInfo.sortType];
+            if (_.isNumber(attrVal) && !Number.isInteger(attrVal)) { attrVal = attrVal.toFixed(2); }
+            html += '<li><b>Name: ' + '</b> ' + node.attr[scope.labelAttr] + '</li>';
+            // Don't duplicate 'Name' as it is always displayed as first property in tooltip info
+            if (attrTitle !== 'Name') {
+                html += '<li><b>' + _.startCase(attrTitle) + '</b> ' + attrVal + '</li>';
+            }
+            html += '</ul>';
+            return html;
+        };
 
-                scope.getNodeTooltipHtml = function(node) {
-                    var html = '<ul class="list-unstyled">';
-                    var attrTitle = _.get(_.find(scope.sortTypes, 'id', scope.sortInfo.sortType), 'title', '');
-                    var attrVal = node.attr[scope.sortInfo.sortType];
-                    if (_.isNumber(attrVal) && !Number.isInteger(attrVal)) { attrVal = attrVal.toFixed(2); }
-                    html += '<li><b>Name: ' + ':</b> ' + node.attr[scope.labelAttr] + '</li>';
-                    // Don't duplicate 'Name' as it is always displayed as first property in tooltip info
-                    if (attrTitle !== 'Name') {
-                        html += '<li><b>' + _.startCase(attrTitle) + ':</b> ' + attrVal + '</li>';
-                    }
-                    html += '</ul>';
-                    return html;
-                };
+        scope.getNodeColor = function(node) {
+            if (scope.layout && node && node.attr && node.attr[scope.nodeColorAttr]) {
+                return memoizedGetFunctionColor(node.attr[scope.nodeColorAttr]);
+            }
+        }
 
-                scope.getNodeColor = function(node) {
-                    if (scope.layout && node && node.attr && node.attr[scope.nodeColorAttr]) {
-                        return memoizedGetFunctionColor(node.attr[scope.nodeColorAttr]);
-                    }
-                };
-
-                scope.getGroupColor = function(groupName) {
-                    if (scope.layout) {
-                        return memoizedGetFunctionColor(groupName);
-                    }
-                };
-
-                function getFunctionColor(cluster) {
-                    return d3.rgb(scope.layout.scalers.color(cluster)).toString();
-                }
-
-                function selectNodes(nodeIds, ev) {
-                    parCtrl.replaceSelection();
-                    graphHoverService.clearHovers(ev);
-                    graphSelectionService.selectByIds(nodeIds , 0);
-                    FilterPanelService.rememberSelection(false);
-                }
-
-                function highlightNodes(nodes) {
-                    nodeSelectionService.addNodesToSelected(nodes);
-                    nodeSelectionService.highlightAllSelected(true);
-                }
-
-                function hoverNodes(nodes) {
-                    parCtrl.persistSelection();
-                    // graphHoverService.hoverByIds(_.map(nodes, 'id'), 0, false);
-                    nodeSelectionService.addNodesToTempHighlightedNodes(nodes);
-                    nodeSelectionService.highlightAllSelected(true);
-                }
-
-
-                function unHoverNodes(nodes) {
-                    // graphHoverService.unhoverByIds(_.map(nodes, 'id'));
-                    nodeSelectionService.removeNodesFromTempHighlightedNodes(nodes);
-                    nodeSelectionService.highlightAllSelected(true);
-                }
-
-                function _filterNodes(clusterName) {
-                    return _.filter(scope.nodes, function filterNodesByCluster(node) {
-                        return node && node.attr && node.attr[scope.nodeColorAttr] && node.attr[scope.nodeColorAttr] === clusterName;
-                    });
-                }
+        scope.getGroupColor = function(groupName) {
+            if (scope.layout) {
+                return memoizedGetFunctionColor(groupName);
             }
         }
 
