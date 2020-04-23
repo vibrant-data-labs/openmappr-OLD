6d219b8d0de02a0ea57c15f1f3d466b18723f81f
Jesse Russell <jrussellfreelance@gmail.com>
2020-04-22 20:22:15 -0400

    Update nodeSelectionService.js

--- client/src/components/core/services/nodeSelectionService.js
+++ client/src/components/core/services/nodeSelectionService.js
@@ -82,8 +82,8 @@ angular.module('common')
                 var subsettedNodes = dataGraph.getAllNodes();
                 var nodes = (value != null ? dataGraph.getNodesByAttrib(attr, value, fivePct, subsettedNodes, true) : []);
                 // add 750 ms delay to _hoverHighlightHelper
-                setTimeout(function() { _hoverHighlightHelper(nodes)}, 750);
-                // _hoverHelper(_.map(nodes, 'id'));
+                //setTimeout(function() { _hoverHighlightHelper(nodes)}, 750);
+                _hoverHelper(_.map(nodes, 'id'));
                 // _hoverHighlightHelper(nodes);
             }
             //
