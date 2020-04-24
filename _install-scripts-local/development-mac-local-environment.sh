a63c919ae2a4ce9fcd0348d99ef0bb0a07a4c144
Yale Fox <yalefox@gmail.com>
2020-04-24 16:24:33 -0400

    cleaning up comments on macOS github script

--- _install-scripts-local/development-mac-local-environment.sh
+++ _install-scripts-local/development-mac-local-environment.sh
@@ -1,49 +1,55 @@
 #!/bin/bash
 # 
-# This script assumes you have nothing installed. It will skip over anything you don't need.
 
 ####################
 
+# Installing Homebrew Package Manager
 echo "Installing Homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
+# Installing Node Version Manager 
 echo "Installing Node Version Manager (NVM)"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
-# reload the system environment
+# Reload system environment 
 echo "Reloading system environment"
 source ~/.zshrc
 
-# Install NodeJS 8.12 and switch to it
+# Install NodeJS 8.12
 echo "Installing NodeJS v8.12.0 (openmappr dependency)"
 nvm install 8.12.0
 
+# Switch to NodeJS 8.12 (for openmappr)
 echo "Switching to NodeJS v8.12.0"
 nvm use 8.12.0
 
-# Install dependencies 
-echo "Installing docker"
+# Install Docker 
+echo "Installing Docker"
 brew cask install docker
+
+# Open Docker, it needs to be up during install
+echo "Opening Docker"
 open /Applications/Docker.app
 
-echo "Installing dependencies"
-brew install git curl
+# Install Git
+echo "Installing Git"
+brew install git
 
-# Clone openMappr repo
-echo "Cloning openmappr repo"
+# Clone openmappr Github Repository
+echo "Cloning openmappr Repository from Github"
 git clone https://github.com/selfhostedofficial/openmappr
 cd openmappr
 
-# Install ruby
+# Install Ruby
+echo "Installing Ruby"
 brew install ruby
 
-# Install the compass Gem
-echo "installing compass gem"
-gem install compass
+# Install Ruby Gem Sass
+echo "Installing Ruby Gem: Sass"
+why i
 gem install sass
 
-# npm install -g sass
-
+# Installing Bower & Grunt 
 echo "Installing bower and grunt"
 sudo npm install -g yo bower grunt-cli
 npm install
@@ -53,7 +59,15 @@ bower install
 echo "Building application"
 grunt
 
+# Start the server
 echo "running server at PORT: http://localhost:8080"
 ./run_local_mode.sh
 
+# When you need to re-open the application
+# Navigate to the ROOT of your local github repo
+# Run the following command:
+# 
 # docker-compose -f docker-compose-local.yml up -d
+
+
+
