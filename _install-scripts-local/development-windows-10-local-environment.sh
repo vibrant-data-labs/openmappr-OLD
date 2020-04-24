772a758f395030efd2be5ff7dc977394daee380b
Yale Fox <yalefox@gmail.com>
2020-04-24 04:12:02 -0400

    add windows quickstart scripts

--- _install-scripts-local/development-windows-10-local-environment.sh
+++ _install-scripts-local/development-windows-10-local-environment.sh
@@ -1,5 +1,9 @@
 ####################
-
+#
+#		       This script sets up a local environment on
+#	                 		  Windows 10
+#
+#
 # echo "IChocolatey Install"
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
