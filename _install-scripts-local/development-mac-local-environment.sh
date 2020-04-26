a15028d4091c85cdc04efa539193cd1b099dcfa1
Yale Fox <yalefox@gmail.com>
2020-04-26 12:02:17 -0400

    Update development-mac-local-environment.sh

--- _install-scripts-local/development-mac-local-environment.sh
+++ _install-scripts-local/development-mac-local-environment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# 
+#
 
 ####################
 
@@ -7,13 +7,29 @@
 echo "Installing Homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-# Installing Node Version Manager 
+# Installing Node Version Manager
 echo "Installing Node Version Manager (NVM)"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
-# Reload system environment 
+# Detecting shell
+CURRENT_SHELL=$(echo $SHELL)
+
+# Reload system environment
 echo "Reloading system environment"
-source ~/.zshrc
+case ${CURRENT_SHELL} in
+
+  "/bin/bash" )
+    source ~/.bash_profile
+  ;;
+  "/bin/zsh" )
+    source ~/.zshrc
+  ;;
+
+  *)
+    source ~/.zshrc
+  ;;
+
+esac
 
 # Install NodeJS 8.12
 echo "Installing NodeJS v8.12.0 (openmappr dependency)"
@@ -23,7 +39,15 @@ nvm install 8.12.0
 echo "Switching to NodeJS v8.12.0"
 nvm use 8.12.0
 
-# Install Docker 
+# Install Docker
+DOCKER_PATH=/Applications/Docker.app
+
+if [ -d ${DOCKER_PATH} ] ; then
+    echo "${DOCKER_PATH} exist"
+    brew cask uninstall docker
+    rm -rvf "${DOCKER_PATH}"
+fi
+
 echo "Installing Docker"
 brew cask install docker
 
@@ -46,10 +70,12 @@ brew install ruby
 
 # Install Ruby Gem Sass
 echo "Installing Ruby Gem: Sass"
-why i
-gem install sass
 
-# Installing Bower & Grunt 
+# Install Ruby Gem Sass
+echo "Installing Ruby Gem: Compass"
+sudo gem install sass compass
+
+# Installing Bower & Grunt
 echo "Installing bower and grunt"
 sudo npm install -g yo bower grunt-cli
 npm install
@@ -59,6 +85,12 @@ bower install
 echo "Building application"
 grunt
 
+# Run the Docker based components
+docker-compose -f docker-compose-local.yml up -d
+
+# Sleep to be sure everything in docker up and running
+sleep 10
+
 # Start the server
 echo "running server at PORT: http://localhost:8080"
 ./run_local_mode.sh
@@ -66,8 +98,5 @@ echo "running server at PORT: http://localhost:8080"
 # When you need to re-open the application
 # Navigate to the ROOT of your local github repo
 # Run the following command:
-# 
+#
 # docker-compose -f docker-compose-local.yml up -d
-
-
-
