981302ade556a7b079966033e3c9fb1a5b194398
Jesse <jrussellfreelance@gmail.com>
2020-07-01 17:33:35 -0400

    ubuntu dev script updates

--- install-scripts/development/development-setup-ubuntu.sh
+++ install-scripts/development/development-setup-ubuntu.sh
@@ -18,89 +18,110 @@ CYAN=6
 WHITE=7
 
 # Prompt user to start setup
-tput setaf $CYAN; read -n1 -p "Press Y/y to set up Openmappr for development on Ubuntu >> "  key
+tput setaf $MAGENTA; read -n1 -p "Press Y/y to set up OpenMappr for development on Ubuntu >> "  key
 
 # Exit if not Y/y
 if [[ "$key" != "y" && "$key" != "Y" ]] ; then
-  tput setaf $RED; echo ">> Exiting! "
+  tput setaf $RED; echo ">>> Exiting! "
   tput setaf $WHITE;
   exit
 fi
 
-tput setaf $MAGENTA; echo "
->> Asking for a sudo password..."
+tput setaf $MAGENTA; echo "";
+echo ">>> Asking for a sudo password..."
+tput setaf $YELLOW;
 sudo whoami >/dev/null
 
-tput setaf $MAGENTA; echo ">> Updating sources..."
+sleep 2
+
+tput setaf $MAGENTA; echo ">>> Updating apt sources..."
 tput setaf $YELLOW;
 sudo apt-get update -qq >/dev/null
 
 sleep 2
 
 # Remove any old versions of docker
-tput setaf $MAGENTA; echo ">> Removing old docker versions..."
+tput setaf $MAGENTA; echo ">>> Removing old docker versions..."
 tput setaf $YELLOW;
 sudo apt-get remove docker docker-engine docker.io containerd runc -y >/dev/null
 
+sleep 2
+
 # Install OpenMappr dependencies via apt
-tput setaf $MAGENTA; echo ">> Installing OpenMappr dependencies..."
+tput setaf $MAGENTA; echo ">>> Installing OpenMappr dependencies..."
 tput setaf $YELLOW;
 sudo apt-get install build-essential ruby-full git python -y >/dev/null
 
 sleep 2
 
 # Install Ruby gem: Sass
-tput setaf $MAGENTA; echo ">> Checking for Ruby gem: sass..."
+tput setaf $MAGENTA; echo ">>> Ruby gem: sass"
+tput setaf $CYAN; echo "> Checking for sass..."
 if sass -v | grep -q "Ruby Sass" >/dev/null ; then
-  tput setaf $GREEN; echo "Sass is already installed!"
+  tput setaf $GREEN; echo "> sass is already installed!"
 else
-  tput setaf $CYAN; echo "Installing Ruby gem: sass..."
+  tput setaf $CYAN; echo "> Installing Ruby gem: sass..."
   sudo gem install sass >/dev/null
 fi
 
 sleep 2
 
 # Install Ruby gem: Compass
-tput setaf $MAGENTA; echo ">> Checking for Ruby gem: compass..."
+tput setaf $MAGENTA; echo ">>> Ruby gem: compass"
+tput setaf $CYAN; echo "> Checking for compass..."
 if compass -v | grep -q "Compass" >/dev/null ; then
-  tput setaf $GREEN; echo "Compass is already installed!"
+  tput setaf $GREEN; echo "> compass is already installed!"
 else
-  tput setaf $CYAN; echo "Installing Ruby gem: compass..."
+  tput setaf $CYAN; echo "> Installing Ruby gem: compass..."
   sudo gem install compass
 fi
 
 sleep 2
 
 # Install docker via apt
-tput setaf $MAGENTA; echo ">> Installing docker..."
-tput setaf $YELLOW;
-curl -sSL https://get.docker.com | sudo bash
+tput setaf $MAGENTA; echo ">>> Docker"
+tput setaf $CYAN; echo "> Checking for docker..."
+if docker -v | grep -q "Docker" >/dev/null ; then
+  tput setaf $GREEN; echo "> docker is already installed!"
+else
+  tput setaf $CYAN; echo "> Installing docker..."
+  tput setaf $YELLOW;
+  curl -sSL https://get.docker.com | sudo bash
+  # Start and enable the docker service
+  tput setaf $CYAN; echo "> Setting up the docker service..."
+  tput setaf $YELLOW;
+  sudo systemctl start docker
+  sudo systemctl enable docker
+fi
 
-# Start and enable the docker service
-tput setaf $MAGENTA; echo ">> Setting up the docker service..."
-tput setaf $YELLOW;
-sudo systemctl start docker
-sudo systemctl enable docker
+sleep 2
 
 # Install docker compose
-tput setaf $MAGENTA; echo ">> Installing docker-compose..."
-tput setaf $YELLOW;
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose >/dev/null
-sudo chmod +x /usr/local/bin/docker-compose
+tput setaf $MAGENTA; echo ">>> docker-compose"
+tput setaf $CYAN; echo "> Checking for docker-compose..."
+if docker-compose -v | grep -q "docker-compose" >/dev/null ; then
+  tput setaf $GREEN; echo "> docker-compose is already installed!"
+else
+  tput setaf $CYAN; echo "> Installing docker-compose..."
+  tput setaf $YELLOW;
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose >/dev/null
+  sudo chmod +x /usr/local/bin/docker-compose
+fi
 
 sleep 2
 
 # Install Node Version Manager
-tput setaf $MAGENTA; echo ">> Checking for Node v8.12.0..."
+tput setaf $MAGENTA; echo ">>> nvm, node, npm"
+tput setaf $CYAN; echo "> Checking for node v8.12.0..."
 if node -v | grep -q "v8.12.0" ; then
-  tput setaf $GREEN; echo "Node v8.12.0 is already installed!"
+  tput setaf $GREEN; echo "> node v8.12.0 is already installed!"
 else
-  tput setaf $CYAN; echo "Installing Node Version Manager v0.35.3..."
+  tput setaf $CYAN; echo "> Installing node version manager v0.35.3..."
   tput setaf $YELLOW;
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
   export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
   # Install Node 8.12.0
-  tput setaf $CYAN; echo "Installing and switching to Node v8.12.0..."
+  tput setaf $CYAN; echo "> Installing and switching to node v8.12.0..."
   tput setaf $YELLOW;
   nvm install 8.12.0
   nvm use 8.12.0
@@ -109,52 +130,59 @@ fi
 sleep 2
 
 # Installing yo, bower, and grunt
-tput setaf $MAGENTA; echo ">> Installing global NPM packages: yo, bower, and grunt..."
+tput setaf $MAGENTA; echo ">>> npm global packages"
+tput setaf $CYAN; echo "> Installing yo, bower, and grunt..."
 tput setaf $YELLOW;
 npm install -g yo bower grunt-cli >/dev/null
 
 sleep 2
 
 # Clone Github Repository
-tput setaf $CYAN; echo "Cloning Openmappr repository from Github..."
+tput setaf $MAGENTA;
+echo ">>> OpenMappr repository"
+tput setaf $CYAN; echo "> Checking for existing OpenMappr files..."
 if [ -d "./openmappr" ] ; then
-  tput setaf $GREEN; echo "Repository is already cloned!"
+  tput setaf $GREEN; echo "> OpenMappr is already cloned!"
   cd openmappr
 else
+  tput setaf $CYAN; echo "> Cloning OpenMappr repository from Github..."
   tput setaf $YELLOW;
-  git clone https://github.com/selfhostedworks/openmappr.git
+  git clone https://github.com/selfhostedworks/openmappr.git >/dev/null
   cd openmappr
 fi
 
 sleep 2
 
-tput setaf $CYAN; echo "Running npm and bower install steps..."
-tput setaf $YELLOW; 
+tput setaf $CYAN; echo "> Running npm and bower install steps..."
+tput setaf $YELLOW;
 npm install >/dev/null
 bower install >/dev/null
 
 # Build the application
-tput setaf $CYAN; echo "Building the application with grunt..."
-tput setaf $YELLOW; 
+tput setaf $CYAN; echo "> Building the application with grunt..."
+tput setaf $YELLOW;
 grunt >/dev/null
 
 sleep 2
 
 # Start the docker-compose stack
-tput setaf $MAGENTA; echo ">> Checking for an existing docker compose stack..."
-if sudo docker ps -a | grep -q "openmappr_" &2>/dev/null ; then
-  tput setaf $GREEN; echo "The docker compose stack is already running. Let's remove it and start over..."
-  sudo docker-compose down &1>/dev/null
+tput setaf $MAGENTA; echo ">>> docker-compose stack"
+tput setaf $CYAN; echo "> Checking for an existing docker compose stack..."
+if sudo docker ps -a | grep -q "openmappr_" >/dev/null ; then
+  tput setaf $GREEN; echo "> The docker compose stack is already running. Let's remove it and start over..."
+  tput setaf $YELLOW;
+  sudo docker-compose down >/dev/null
   sudo docker-compose -f docker-compose.local.yml up -d
 else
-  tput setaf $CYAN; echo "Starting local development stack via Docker Compose..."
+  tput setaf $CYAN; echo "> Starting local development stack via docker compose..."
   sudo docker-compose -f docker-compose.local.yml up -d
 fi
 
 sleep 2
 
 # Start the server
-tput setaf $CYAN; echo "Running server at http://localhost:8080 ..."
+tput setaf $MAGENTA; echo ">>> Running server at http://localhost:8080 ..."
+  tput setaf $YELLOW;
 ./run_local_mode.sh
 
 # Set default color back to white
