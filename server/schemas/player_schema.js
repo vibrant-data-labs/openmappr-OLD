f6d7ad8848b85fd7d924db365f7d9a1b08f72b81
mike.tulubaev <mike.tulubaev@sharp-dev.net>
2021-02-05 22:56:25 +0300

    add configuration of node neighborhood mechanism

--- server/schemas/player_schema.js
+++ server/schemas/player_schema.js
@@ -23,6 +23,7 @@ var PlayerSchema = new mongoose.Schema({
         modalIntroHtml: String,
         modalTitle:     String,
         searchAlg:      String,
+        neighborhoodDegree: Number,
         startPage:      String,
         modalSubtitle:  String,
         modalLogo:      String,
